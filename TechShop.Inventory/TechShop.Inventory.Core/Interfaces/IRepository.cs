03ebfba1a45f1124d6b67803c4cc715b1319ae3c
skinofme <skinoffme@gmail.com>
2026-04-05 07:56:35 -0600

    feat(Core): add interface for repositories

---
+++ TechShop.Inventory/TechShop.Inventory.Core/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TechShop.Inventory.Core.Interfaces
+{
+	public interface IRepository<T> where T : class
+	{
+		Task<List<T>> GetAllAsync();
+		Task<T> GetByIdAsync(int id);
+		Task AddAsync(T entity);
+		Task EditAsync(T entity, DateTime now);
+		Task<bool> DeleteAsync(int id);
+	}
+}
