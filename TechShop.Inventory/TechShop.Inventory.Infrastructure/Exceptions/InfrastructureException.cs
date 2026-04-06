bfdb3ff8e594fe1dfb7ea04fc4b33126b5e33af9
skinofme <skinoffme@gmail.com>
2026-04-06 16:12:04 -0600

    feat(Infrastructure): add main Insfrastructure exception
    StockItemNotFoundException has also been added

---
+++ TechShop.Inventory/TechShop.Inventory.Infrastructure/Exceptions/InfrastructureException.cs
@@ -0,0 +1,17 @@
+﻿namespace TechShop.Inventory.Infrastructure.Exceptions
+{
+	public abstract class InfrastructureException : Exception
+	{
+		public InfrastructureException() 
+		{
+		}
+
+		public InfrastructureException(string message) : base(message)
+		{
+		}
+
+		public InfrastructureException(string message, Exception innerException) : base(message, innerException) 
+		{
+		}
+	}
+}
---
+++ TechShop.Inventory/TechShop.Inventory.Infrastructure/Exceptions/StockItemRepository/StockItemNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechShop.Inventory.Infrastructure.Exceptions.StockItemRepository
+{
+	public class StockItemNotFoundException : InfrastructureException
+	{
+		public StockItemNotFoundException(int idStockItem) : base($"The source requested: {idStockItem} cannot be found.")
+		{ 
+		}
+	}
+}
